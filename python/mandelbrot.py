a917f2a14945937545a9bb8f5bfdf3200d7cdc78
Marcelo Abiarraj <marceloat01@gmail.com>
2022-01-23 01:49:22 -0300

    Add colors to mandelbrot and ship variation

--- python/mandelbrot.py
+++ python/mandelbrot.py
@@ -1,29 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
+def ship(p,c):
+    return pow((abs(p.real) + complex(0,1)*abs(p.imag)), 2) + c
+
+def simple(p,c):
+    return p*p + c
 
 def converge(c):
     def func(c,p,it):
-        if it > 100:
-            return True
-        elif abs(p) >= 2:
-            return False
+        if it > 64 or abs(p) >= 2:
+            return it
         else:
-            next = p*p + c
-            return func(c,next,it+1)
+            return func(c,ship(p,c),it+1)
     return func(c,c,0)
 
 def setData():
     complexes = []
-    rg = np.linspace(-2,2,900)
-    for i in rg:
-        for j in rg:
+    for i in np.linspace(-2,1,900):
+        for j in np.linspace(-1.5,1.5,900):
             c = complex(i,j)
-            if converge(c):
-                complexes.append(c)
+            iterations = math.log(converge(c)+1)
+            complexes.append([c, iterations])
     return np.array(complexes)
 
 data = setData()
-x = data.real
-y = data.imag
-plt.plot(x,y, 'g*')
+x = data[:,0].real
+y = data[:,0].imag
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(x,y, c=data[:,1])
 plt.show()
